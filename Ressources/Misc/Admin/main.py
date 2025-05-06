87175d97759114913e9cc651a161d203b8b5da82
David-Bieber <david.f.bieber@gmail.com>
2025-05-06 10:38:15 +0200

    add changelog?

--- Ressources/Misc/Admin/main.py
+++ Ressources/Misc/Admin/main.py
@@ -1,30 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import random
+from collections import defaultdict
 
 class chunk_object:
     def __init__(self, pos_x, pos_y, pos_z, s_x = 1, s_y = 1, s_z = 1):
         self.size = [s_x, s_y, s_z]
         self.pos = [pos_x, pos_y, pos_z]
-
+        self.ident = (f'{pos_x}-{pos_y}-{pos_z}')
     
 
 
 def create_grid(x, y, z = 1):
-    # used to create a grid as base for the world
-    grid = [[[chunk_object(x_, y_, z_, ) for z_ in range(z)] for y_ in range(y)] for x_ in range(x)]
+
+    # creating base grid
+
+    grid = {}
     
-    for i in grid:
-        for j in i:
-            for k in j:
-                print(f'pos: {k.pos} size: {k.size}')
+    for x_ in range(x):
+        for y_ in range(y):
+            for z_ in range(z):
+
+                #print(f'pos: {x_},{y_},{z_}')
+                chunk = chunk_object(x_, y_, z_)
+                ident = (f'{x_}-{y_}-{z_}')
+                grid[ident] = chunk
+
 
     return grid
+
+def render_chunk_object(chunk: chunk_object):
+    chunk_pos = chunk.pos
+    chunk_size = chunk.size
+    fig = plt.figure()
+    ax = fig.add_subplot((chunk_size[0],chunk_size[1],chunk_size[2]), projection='3d')
 
 
 
 # print(create_grid(10, 10))
 
-create_grid(5, 5, 5)
+grid = create_grid(3, 3, 3)
 
-plt.imshow(create_grid(10, 10))
+for i in grid:
+    # position output
+    print(f'position: {grid[i].pos} size: {grid[i].size}')
