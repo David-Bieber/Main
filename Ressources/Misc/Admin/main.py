182e47998be1253e2de0524fb1be995e9a60688b
David-Bieber <david.f.bieber@gmail.com>
2025-05-06 11:09:16 +0200

    .

--- Ressources/Misc/Admin/main.py
+++ Ressources/Misc/Admin/main.py
@@ -4,11 +4,14 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import random
 from collections import defaultdict
 
-class chunk_object:
+class ChunkObject:
     def __init__(self, pos_x, pos_y, pos_z, s_x = 1, s_y = 1, s_z = 1):
         self.size = [s_x, s_y, s_z]
         self.pos = [pos_x, pos_y, pos_z]
-        self.ident = (f'{pos_x}-{pos_y}-{pos_z}')
+        self.ident = (pos_x, pos_y, pos_z)
+
+    def __repr__(self):
+        return f"Chunk '{self.ident}' at coordinates {self.pos}, size is {self.size})"
     
 
 
@@ -23,14 +26,13 @@ def create_grid(x, y, z = 1):
             for z_ in range(z):
 
                 #print(f'pos: {x_},{y_},{z_}')
-                chunk = chunk_object(x_, y_, z_)
-                ident = (f'{x_}-{y_}-{z_}')
-                grid[ident] = chunk
+                chunk = ChunkObject(x_, y_, z_)
+                grid[chunk.ident] = chunk
 
 
     return grid
 
-def render_chunk_object(chunk: chunk_object):
+def render_chunk_object(chunk: ChunkObject):
     chunk_pos = chunk.pos
     chunk_size = chunk.size
     fig = plt.figure()
@@ -44,4 +46,5 @@ grid = create_grid(3, 3, 3)
 
 for i in grid:
     # position output
-    print(f'position: {grid[i].pos} size: {grid[i].size}')
+    print(grid[i])
+    # print(f'position: {grid[i].pos} size: {grid[i].size}')
